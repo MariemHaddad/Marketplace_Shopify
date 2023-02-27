96ce32a9077ed48c544c5307e3f6ead7c40c23be
Mariem <mariem.haddad@esprit.tn>
2023-02-27 23:33:34 +0100

    Update Adresse.java

--- src/main/java/com/example/marketplace/Entities/Adresse.java
+++ src/main/java/com/example/marketplace/Entities/Adresse.java
@@ -8,17 +8,16 @@ import java.io.Serializable;
 import java.util.List;
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Adresse implements Serializable {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="adreese1")
     private String adresse1;
-  /* @Column(name="adreese2")
-    private String adresse2;*/
+    /* @Column(name="adreese2")
+     private String adresse2;*/
     @Column(name="ville")
     private String ville;
     @Column(name="codePostale")
